8ae500c9b0279f61bc376f8e89fe86eb32c79576
Shigeru Hagiwara <hgwrsgr@gmail.com>
2016-11-19 02:03:16 +0900

    add example test

--- test/bayes/bayesian_test.rb
+++ test/bayes/bayesian_test.rb
@@ -122,4 +122,13 @@ class BayesianTest < Test::Unit::TestCase
     classification_after_untrain = @classifier.classify 'seven'
     assert_not_equal classification_of_bad_data, classification_after_untrain
   end
+
+  def test_example
+    @classifier.train_interesting "here are some good words. I hope you love them"
+    @classifier.train_uninteresting "here are some bad words, I hate you"
+    @classifier.classify "I hate bad words and you" # returns 'Uninteresting'
+    classifier_snapshot = Marshal.dump @classifier
+    trained_classifier = Marshal.load classifier_snapshot
+    trained_classifier.classify "I love" # returns 'Interesting'
+  end
 end
